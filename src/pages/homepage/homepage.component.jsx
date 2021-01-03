b9c9dde17810be9d4aafaa3f4f111f610125c357
shivasaigujjari <shivagujjari95@gmail.com>
2021-01-03 16:31:58 +0530

    Menu item is styled

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './homepage.styles.scss';
+import Directory from "../../components/directory/directory.components.jsx";
+
+const HomePage =() =>(
+    <div className='homepage'>
+      <Directory/>
+    </div>
+)
+export default HomePage;
